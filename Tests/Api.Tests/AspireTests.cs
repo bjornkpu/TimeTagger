523f9ae1a4f867dddde178e84c9ffa5c3aaa9a80
Bjørn Kristian Punsvik <bk.punsvik@gmail.com>
2024-07-09 23:26:13 +0200

    feat: Update AspireTests and remove unused test files

--- Tests/Api.Tests/AspireTests.cs
+++ Tests/Api.Tests/AspireTests.cs
@@ -1,10 +1,20 @@
 ﻿using System.Net;
+using System.Net.Http.Json;
+using Api.Features.Record.Post;
 using Aspire.Hosting.Testing;
+using Xunit.Abstractions;
 
 namespace Api.Tests;
 
 public class AspireTests
 {
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public AspireTests(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
     [Fact]
     public async Task GetApiResourceRootReturnsOkStatusCode()
     {
@@ -19,5 +29,65 @@ public class AspireTests
 
         // Assert
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task PostRecord_WithoutTag_ReturnsOk()
+    {
+        // Arrange
+        var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.AppHost>();
+        await using var app = await appHost.BuildAsync();
+        await app.StartAsync();
+
+        // Act
+        var httpClient = app.CreateHttpClient("api");
+        var response = await httpClient.PostAsJsonAsync("/record", new Request());
+
+        Print<Response>(response);
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task PostRecord_WithTag_ReturnsOk()
+    {
+        // Arrange
+        var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.AppHost>();
+        await using var app = await appHost.BuildAsync();
+        await app.StartAsync();
+
+        var tag = "Work";
+
+        // Act
+        var httpClient = app.CreateHttpClient("api");
+        var response = await httpClient.PostAsJsonAsync("/record", new Request()
+        {
+            Tag = tag
+        });
+
+
+        var responseBody = await response.Content.ReadFromJsonAsync<Response>();
+        Assert.NotNull(responseBody);
+
+        Print(responseBody);
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.Equal(tag, responseBody.Tag);
+    }
+
+    private async void Print<T>(HttpResponseMessage response)
+    {
+        var responseBody = await response.Content.ReadFromJsonAsync<T>();
+        var jsonSerializerOptions = new System.Text.Json.JsonSerializerOptions { WriteIndented = true };
+        var json = System.Text.Json.JsonSerializer.Serialize(responseBody, jsonSerializerOptions);
+        _testOutputHelper.WriteLine(json);
+    }
+    private async void Print<T>(T responseBody)
+    {
+        var jsonSerializerOptions = new System.Text.Json.JsonSerializerOptions { WriteIndented = true };
+        var json = System.Text.Json.JsonSerializer.Serialize(responseBody, jsonSerializerOptions);
+        _testOutputHelper.WriteLine(json);
     }
 }
--- Tests/Api.Tests/Global.cs
+++
@@ -1,5 +0,0 @@
-﻿global using FastEndpoints.Testing;
-global using Microsoft.VisualStudio.TestPlatform.TestHost;
-global using Xunit.Priority;
-global using FastEndpoints;
-global using FluentAssertions;
