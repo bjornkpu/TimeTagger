1d437bfb6e451d2bf79f4e223bf34bf84dcc2e7f
Bjørn Kristian Punsvik <bk.punsvik@gmail.com>
2024-07-09 22:47:11 +0200

    feat: Enhance Api.Tests using aspire style

--- Tests/Api.Tests/App.cs
+++ Tests/Api.Tests/App.cs
@@ -1,3 +1,29 @@
-﻿namespace Api.Tests;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
-public class App : AppFixture<Program>;
+namespace Api.Tests;
+
+public class App : AppFixture<Program>
+{
+    protected override Task SetupAsync()
+    {
+        // place one-time setup code here
+        return Task.CompletedTask;
+    }
+
+    protected override void ConfigureApp(IWebHostBuilder builder)
+    {
+        // do host builder config here
+    }
+
+    protected override void ConfigureServices(IServiceCollection s)
+    {
+        // do test service registration here
+    }
+
+    protected override Task TearDownAsync()
+    {
+        // do cleanups here
+        return Task.CompletedTask;
+    }
+}
---
+++ Tests/Api.Tests/AspireTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using Aspire.Hosting.Testing;
+
+namespace Api.Tests;
+
+public class AspireTests
+{
+    [Fact]
+    public async Task GetApiResourceRootReturnsOkStatusCode()
+    {
+        // Arrange
+        var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.AppHost>();
+        await using var app = await appHost.BuildAsync();
+        await app.StartAsync();
+
+        // Act
+        var httpClient = app.CreateHttpClient("api");
+        var response = await httpClient.GetAsync("/");
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+}
