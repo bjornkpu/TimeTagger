0bd487f53a676f6b3bb3615d815881c6fea00531
Bjørn Kristian Punsvik <bjorn.kristian.punsvik@tietoevry.com>
2024-08-07 13:26:51 +0200

    feat(login): add login endpoint and tests
    - Implement `/login` endpoint using FastEndpoints
    - Add initial unit tests for login functionality

---
+++ Src/Api/Features/Login/Endpoint.cs
@@ -0,0 +1,52 @@
+﻿using FastEndpoints;
+using FastEndpoints.Security;
+
+namespace Api.Features.Login;
+
+public class UserLoginEndpoint : Endpoint<Request,Response>
+{
+    public override void Configure()
+    {
+        Post("/login");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(Request req, CancellationToken ct)
+    {
+        if (!await AuthService.CredentialsAreValid(req.Email, req.Password, ct))
+        {
+            ThrowError("The supplied credentials are invalid!");
+        }
+
+        var jwtToken = JwtBearer.CreateToken(
+            o =>
+            {
+                o.SigningKey = "2D9CECE5-5E76-461D-98A3-2B1E9261B2ED";
+                o.ExpireAt = DateTime.UtcNow.AddDays(1);
+                o.User.Roles.Add("Manager", "Auditor");
+                o.User.Claims.Add(("UserName", req.Email));
+                o.User["UserId"] = "001"; //indexer based claim setting
+            });
+
+        await SendAsync(new Response{ Token = jwtToken}, cancellation: ct);
+    }
+}
+
+public record Request
+{
+    public required string Email { get; init; }
+    public required string Password { get; init; }
+}
+
+public record Response
+{
+    public required string Token { get; init; }
+}
+
+public static class AuthService
+{
+    public static async Task<bool> CredentialsAreValid(string email, string password, CancellationToken ct)
+    {
+        return true;
+    }
+}
---
+++ Tests/Api.Tests/LoginTests.cs
@@ -0,0 +1,54 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json;
+using Api.Features.Login;
+using Xunit.Abstractions;
+
+namespace Api.Tests;
+
+public class LoginTests(TestFixture fixture, ITestOutputHelper _testOutputHelper)
+    : IClassFixture<TestFixture>
+{
+    private readonly HttpClient _httpClient = fixture.httpClient;
+
+    [Fact]
+    public async Task PostLogin_ReturnsOk()
+    {
+        // Arrange
+        const string tag = "Work";
+
+        // Act
+        var response = await _httpClient.PostAsJsonAsync("/login", new Request
+        {
+            Email = "a@b.c",
+            Password = "GoodPassword123"
+        });
+
+        // Assert
+        var responseBody = await response.Content.ReadFromJsonAsync<Response>();
+        Assert.NotNull(responseBody);
+        Print(responseBody);
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.NotNull(responseBody.Token);
+
+        var jwtSecurityToken = new JwtSecurityTokenHandler().ReadJwtToken(responseBody.Token);
+        _testOutputHelper.WriteLine(JsonSerializer.Serialize(jwtSecurityToken.Payload,
+            new JsonSerializerOptions { WriteIndented = true }));
+    }
+
+    private async void Print<T>(HttpResponseMessage response)
+    {
+        var responseBody = await response.Content.ReadFromJsonAsync<T>();
+        var jsonSerializerOptions = new JsonSerializerOptions { WriteIndented = true };
+        var json = JsonSerializer.Serialize(responseBody, jsonSerializerOptions);
+        _testOutputHelper.WriteLine(json);
+    }
+
+    private async void Print<T>(T responseBody)
+    {
+        var jsonSerializerOptions = new JsonSerializerOptions { WriteIndented = true };
+        var json = JsonSerializer.Serialize(responseBody, jsonSerializerOptions);
+        _testOutputHelper.WriteLine(json);
+    }
+}
